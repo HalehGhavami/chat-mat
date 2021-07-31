949bf63105114f438116b6d101cce2941f4a5ba8
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-07-31 12:05:27 +0200

    imported Gifted chat and KeyboardAvoidingView  + added system message

--- components/Chat.js
+++ components/Chat.js
@@ -1,23 +1,65 @@
 import React, { Component } from 'react';
-import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
+// import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
+
+// import Gifted Chat
+import { GiftedChat } from 'react-native-gifted-chat';
+
+// import KeyboardAvoidingView is only necessary for users running the app on Android
+import { View, Platform, KeyboardAvoidingView } from 'react-native';
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 2,
+          text: 'This is a system message',
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
   render() {
     let name = this.props.route.params.name;
-    // OR ...
-    // let { name } = this.props.route.params;
 
     // display user's name in navigation bar at the top of Chat
     this.props.navigation.setOptions({ title: name });
-
     return (
-      <View
-        style={{ flex: 1, backgroundColor: this.props.route.params.backColor }}
-      >
-        <Button
-          title="Go to Start"
-          onPress={() => this.props.navigation.navigate('Start')}
+      <View style={{ flex: 1 }}>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
         />
+        {Platform.OS === 'android' ? (
+          <KeyboardAvoidingView behavior="height" />
+        ) : null}
       </View>
     );
   }
